6dc538c2bb96225d45c13d614db0199b8a9d1bb4
WozzyaQ <officialwozzya@gmail.com>
2021-05-14 23:39:37 +0300

    feat: add new role ON_HOLD and ADMIN  for future usage. every new registered worker is marked with role ON_HOLD meaning that ADMIN shold approve the worker and give hime an appropriate role

--- src/main/java/com/epam/ua/wozzya/model/dao/entity/enumeration/Role.java
+++ src/main/java/com/epam/ua/wozzya/model/dao/entity/enumeration/Role.java
@@ -1,5 +1,5 @@
 package com.epam.ua.wozzya.model.dao.entity.enumeration;
 
 public enum Role {
-    CASHIER, COMMODITY_EXPERT, SENIOR_CASHIER;
+    CASHIER, COMMODITY_EXPERT, SENIOR_CASHIER, ON_HOLD, ADMIN;
 }
